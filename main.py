559d96518a5faee84ecab761d9e25d9eb314a615
TrainingBasket <sourcecode@trainingbasket.in>
2020-05-25 15:08:06 +0530

    Added delete and add record functionality

--- main.py
+++ main.py
@@ -48,6 +48,37 @@ def specific(b_id):
 	return render_template('specific.html',row=row)
 
 
+@app.route('/delete/<int:b_id>',methods = ['GET','POST'])
+def delete(b_id):
+	if request.method == 'POST':
+		with sqlite3.connect('books.db') as conn:
+			cur = conn.cursor()
+			sql = 'DELETE FROM BOOK WHERE ID ='+str(b_id)
+			cur.execute(sql)
+		return redirect(url_for('jobs'))
+	return render_template('delete.html',b_id = b_id)
+
+@app.route('/addrecord',methods=['GET','POST'])
+def addrecord():
+	if request.method == 'POST':
+		try:
+			with sqlite3.connect('books.db') as conn:
+				cur = conn.cursor()
+				b_id = int(request.form['id'])
+				b_name = request.form['b_name']
+				b_author =request.form['b_author']
+				year = int(request.form['b_year'])
+				isbn = int(request.form['b_isbn'])
+
+				v = str((b_id,b_name,b_author,year,isbn))
+				sql = "INSERT INTO BOOK VALUES " + v
+				cur.execute(sql)
+				conn.commit()
+			return redirect(url_for('jobs'))
+		except:
+			return redirect(url_for('jobs'))
+	return render_template('addrecord.html')
+
 @app.route('/api/v1/checkprime')
 def check_prime():
 	if 'num' in request.args:
